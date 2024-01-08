4c64e7cecfd415d6b03cdab4953b5d9a7766376d
mihaipaulorban <mihaipaulorban@gmail.com>
2024-01-08 17:15:23 +0100

    feat: php con variabili e censura

--- censor.php
+++ censor.php
@@ -1,1 +1,16 @@
-<?php?>
+<?php
+// Variabili
+$paragraph = $_POST['paragraph'];
+$word = $_POST['word'];
+
+// Paragrafo senza censura e la sua lunghezza
+echo "<p>Paragrafo originale: " . htmlspecialchars($paragraph) . "</p>";
+echo "<p>Lunghezza paragrafo: " . strlen($paragraph) . "</p>";
+
+// Sostituzione parola
+$censoredParagraph = str_replace($word, '***', $paragraph);
+
+// Paragrafo censurato e la sua lunghezza
+echo "<p>Paragrafo censurato: " . htmlspecialchars($censoredParagraph) . "</p>";
+echo "<p>Lunghezza paragrafo censurato: " . strlen($censoredParagraph);
+?>
