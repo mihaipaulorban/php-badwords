3fad30437e3bd5279329ec7b1247d3fcba4c7047
mihaipaulorban <mihaipaulorban@gmail.com>
2024-01-08 16:23:58 +0100

    feat: impostazione file e struttura index php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Badwords</title>
+</head>
+<body>
+
+<!-- Form per il programmino -->
+    <form action="censor.php" method="post">
+
+        <!-- Textarea con paragrafo -->
+        <label for="paragraph">Paragrafo:</label><br>
+        <textarea id="paragraph" name="paragraph" rows="4" cols="50"></textarea><br>
+
+        <!-- Parola che poi verrà censurata -->
+        <label for="word">Parola da censurare:</label><br>
+        <input type="text" id="word" name="word"><br><br>
+        <input type="submit" value="Invia">
+    </form>
+</body>
+</html>
